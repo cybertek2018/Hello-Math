36981f79b12ccadf4eeb519ff4a288cd1ce0b5f7
yagshy <yagshy@192.168.0.103>
2018-06-09 14:34:45 -0500

    even numbers up to 100

---
+++ src/com/oddnumbers/EvenNumbers.java
@@ -0,0 +1,15 @@
+package com.oddnumbers;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		
+		for(int k=0; k<=100; k+=2) { 
+			
+			
+			System.out.print(k+" ");
+		}
+
+	}
+
+}
